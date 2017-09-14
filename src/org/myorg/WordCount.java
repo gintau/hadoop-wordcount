7054fd1b4e6697371c0d284c8a8f9d851e8db82f
Ting-Kuan Wu <tkwu@cloudera.com>
2017-09-14 08:19:08 -0700

    fail the job with non-0 code.

--- src/org/myorg/WordCount.java
+++ src/org/myorg/WordCount.java
@@ -1,8 +1,8 @@
 package org.myorg;
-        
+
 import java.io.IOException;
 import java.util.*;
-        
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
@@ -11,13 +11,13 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-        
+
 public class WordCount {
-        
+
  public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
-        
+
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String line = value.toString();
         StringTokenizer tokenizer = new StringTokenizer(line);
@@ -26,11 +26,11 @@ public class WordCount {
             context.write(word, one);
         }
     }
- } 
-        
+ }
+
  public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-    public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
       throws IOException, InterruptedException {
         int sum = 0;
         for (IntWritable val : values) {
@@ -39,7 +39,7 @@ public class WordCount {
         context.write(key, new IntWritable(sum));
     }
  }
-        
+
  public static void main(String[] args) throws Exception {
 
     Configuration actionConf = new Configuration(false);
@@ -47,20 +47,22 @@ public class WordCount {
 
     Job job = new Job(actionConf, "wordcount");
     job.setJarByClass(WordCount.class);
-    
+
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
-        
+
     job.setMapperClass(Map.class);
     job.setReducerClass(Reduce.class);
-        
+
     job.setInputFormatClass(TextInputFormat.class);
     job.setOutputFormatClass(TextOutputFormat.class);
-        
+
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        
-    job.waitForCompletion(true);
+
+    if (!job.waitForCompletion(true)) {
+      System.exit(1);
+    }
  }
-        
+
 }
